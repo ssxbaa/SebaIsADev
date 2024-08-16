3d2b4b566c9f71d06ce878c05f24930445d73239
Sebastiano <ssxbaa@proton.me>
2024-08-17 01:21:14 +0200

    Add files via upload

---
+++ js/getDiscordStatus.js
@@ -0,0 +1,29 @@
+const sebaId = "1213198625610408046";
+const url = "https://api.lanyard.rest/v1/users/" + sebaId;
+
+function fetchAndUpdate() {
+
+    const statusTexts = {
+        "online": "online",
+        "dnd": "online (dnd)",
+        "idle": "idle",
+        "offline": "offline"
+    };
+
+    fetch(url)
+
+        .then(info => info.json())
+
+        .then(data => {
+
+            const status = data.data.discord_status;
+            const descParagraph = document.getElementById("desc");
+            descParagraph.innerHTML += `<br>currently <span class="${status}">${statusTexts[status]}</span>`;
+            
+        })
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchAndUpdate();
+});
